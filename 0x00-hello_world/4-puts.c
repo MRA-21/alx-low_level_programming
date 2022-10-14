f2ab4b196fb24c9dec544de274f9c6d53c343c47
mubarak <rajimubarak6@gmail.com>
2022-10-14 09:32:02 -0700

    84

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
  * main - Entry
  * Return: Always 0
@@ -7,5 +7,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
